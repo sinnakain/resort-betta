d72fe4356162282172a1d9570cabcee36f967c46
Акимов Севастьян <akimov.s.iv@omega.sbrf.ru>
2020-05-16 14:10:53 +0300

    add new tags and apartments

--- core/tags.php
+++ core/tags.php
@@ -12,7 +12,63 @@ function get_tags()
         'bed' => [
             'title' => 'Односпальная кровать',
             'icon' => '#bed'
-        ]
+        ],
+        'sofa' => [
+            'title' => 'Кресло-кровать',
+            'icon' => '#sofa'
+        ],
+        'parking' => [
+            'title' => 'Бесплатная парковка',
+            'icon' => '#parking'
+        ],
+        'bathroom' => [
+            'title' => 'Ванная комната',
+            'icon' => '#bathroom'
+        ],
+        'wifi' => [
+            'title' => 'Wi-Fi',
+            'icon' => '#wifi'
+        ],
+        'conditioner' => [
+            'title' => 'Кондиционер',
+            'icon' => '#conditioner'
+        ],
+        'washer' => [
+            'title' => 'Доступ к стиральной машине',
+            'icon' => '#washer'
+        ],
+        'pet' => [
+            'title' => 'Размещение с питомцами',
+            'icon' => '#pet'
+        ],
+        'balcony' => [
+            'title' => 'Балкон',
+            'icon' => '#balcony'
+        ],
+        'loggia' => [
+            'title' => 'Лоджия',
+            'icon' => '#loggia'
+        ],
+        'veranda' => [
+            'title' => 'Веранда',
+            'icon' => '#veranda'
+        ],
+        'kitchen' => [
+            'title' => 'Просторная кухня',
+            'icon' => '#kitchen'
+        ],
+        'kitchen_2' => [
+            'title' => 'Просторная кухня',
+            'icon' => '#kitchen_2'
+        ],
+        'big_sofa' => [
+            'title' => 'Двуспальный диван',
+            'icon' => '#big_sofa'
+        ],
+        'big_sofa_2' => [
+            'title' => 'Двуспальный диван',
+            'icon' => '#big_sofa_2'
+        ],
     ];
 }
 
@@ -45,7 +101,7 @@ function render_tags($tag_ids, $wrap_items = true)
 {
     echo $wrap_items ? '<div class="tags__container">' : '';
 
-    foreach($tag_ids as $k => $tag_id) {
+    foreach ($tag_ids as $k => $tag_id) {
         render_tag($tag_id);
     }
 
